5afe89c35f7d309b9c56700af83ddf1b4731fc31
Igor Novikov <novikovtoptech@gmail.com>
2023-07-04 09:25:20 +0300

    dz5

--- app/Console/Commands/FirstCommand.php
+++ app/Console/Commands/FirstCommand.php
@@ -25,21 +25,41 @@ class FirstCommand extends Command
      */
     public function handle()
     {
-        if ($this->ask("Скільки тобі років?") < 18) {
+        $user_data['name'] = $this->argument('name');
+
+        if (empty($user_data['name'])) {
+            $this->error('Вкажіть ваше імя при визові комманди!');
+            return;
+        }
+
+        if ($this->ask("Скільки тобі років {$user_data['name']}?") < 18) {
             if ($this->confirm("Ви бажаете продовжити?") === false) {
                 $this->error("Бай бай!");
                 return;
             }
         }
 
-        $name = $this->choice(
-            'Виберіть дію',
-            ['Читання', 'Запис'],
-            0
-        );
+        $action = $this->choice('Виберіть дію', ['Читання', 'Запис'], 0);
 
-        $this->info($name);
+        if ($action == 'Запис') {
+            $user_data['gender'] = $this->choice('Ваша стать?', ['Жінка', 'Чоловік'], 1);
+            $user_data['city'] = $this->ask("Ваше місто?");
+            $user_data['phone'] = $this->ask("Ваше номер телефону?");
+            $write = file_put_contents('./file.txt', json_encode($user_data, JSON_UNESCAPED_UNICODE));
+            if($write !== false) {
+                $this->info("Файл успішно збережено!");
+                return;
+            }
+            $this->info("Помилка запису файлу!");
 
+
+        } elseif ($action == 'Читання') {
+            if (file_exists('./file.txt')) {
+                $this->info(file_get_contents('./file.txt'));
+            } else {
+                $this->error("Файлу не існує! ");
+            }
+        }
 
 
     }
